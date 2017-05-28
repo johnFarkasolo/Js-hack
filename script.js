8b994611882e7699ecf090fce1d0267eee98c8d0
Ivan Yarovyi <farkasolo@mail.ru>
2017-05-28 16:38:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-$(function() {
-	$('.carousel').carousel({
-	  interval: 3000
-	});
-
-	$('.search').on('click', function(){
-		$('.main-menu .navbar-form').slideToggle();
-	});
-});
 
 $(window).load(function(){
-	var carouselCaptionWidth = $('#carousel-sidebar .active img').width();
-	$('#carousel-sidebar img').each(function(){
-		$(this).attr('width', carouselCaptionWidth);
+	var NameVariable = $('#For-id .class img').width();
+	$('#For-id img').each(function(){
+		$(this).attr('width', NameVariable);
 	});
-	$('#carousel-sidebar .sidebar-carousel-caption ').css('max-width', carouselCaptionWidth + 'px');
-	$('#carousel-sidebar .carousel-indicators ').css('max-width', carouselCaptionWidth + 'px');
+	$('#For-id .Class that change').css('max-width', NameVariable + 'px');
 });
